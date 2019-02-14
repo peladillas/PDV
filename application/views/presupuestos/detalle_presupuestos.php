b9d84421103753feb46dc1614fdd64f2f5520e20
diego <diego.nieto@sitrack.com>
2019-02-14 16:50:06 -0300

    sacar $texto

--- application/views/presupuestos/detalle_presupuestos.php
+++ application/views/presupuestos/detalle_presupuestos.php
@@ -15,36 +15,29 @@
 <div class="container">
 <div class="col-md-12">
 	<div class="panel panel-primary">
-		<div class="panel-heading"><?php echo $texto['empresa_titulo'] ?></div>
+		<div class="panel-heading"><?php echo lang('empresa_titulo') ?></div>
 		<div class="panel-body">
 			
 			<div id="printableArea">
 			<?php 
-			if($presupuestos)
-			{
+			if($presupuestos) {
 				echo "<table class='table table-hover'>";
-				foreach ($presupuestos as $row) 
-				{
-					foreach ($impresiones as $impresion) 
-	  				{
+				foreach ($presupuestos as $row) {
+					foreach ($impresiones as $impresion) {
 	  					$clientes	= $this->clientes_model->select($row->id_cliente);
-                        if($clientes)
-						{
-							foreach ($clientes as $row_cliente)
-							{
+                        if($clientes) {
+							foreach ($clientes as $row_cliente) {
 								$nombre = $row_cliente->nombre;
 								$apellido = $row_cliente->apellido;
 							}
 						}
                         
                         $_vendedor   = $this->vendedores_model->select($row->id_vendedor);
-                        if($_vendedor)
-                        {
-                            foreach ($_vendedor as $row_vendedor)
-                            {
+                        if($_vendedor) {
+                            foreach ($_vendedor as $row_vendedor) {
                                 $vendedor = $row_vendedor->vendedor;
                             }
-                        }else{
+                        } else {
                            $vendedor = ""; 
                         }
                         
@@ -55,21 +48,15 @@
 						$cabecera = str_replace("#presupuesto_fecha#", date('d-m-Y', strtotime($row->fecha)), $cabecera);
 						$cabecera = str_replace("#presupuesto_monto#", $row->monto, $cabecera);
                         $cabecera = str_replace("#vendedor#", $vendedor, $cabecera);
-						if(isset($nombre))
-						{
+						if(isset($nombre)) {
 							$cabecera = str_replace("#cliente_nombre#", $nombre, $cabecera);
-						}
-						else
-						{
+						} else {
 							$cabecera = str_replace("#cliente_nombre#", '', $cabecera);
 						}
 						
-						if(isset($apellido))
-						{
+						if(isset($apellido)) {
 							$cabecera = str_replace("#cliente_apellido#", $apellido, $cabecera);
-						}
-						else
-						{
+						} else {
 							$cabecera = str_replace("#cliente_apellido#", '', $cabecera);
 						}
 						
@@ -79,109 +66,96 @@
 						$id_presupuesto = $row->id_presupuesto;
 					}
 				
-				echo "<hr>";
-				
-				$total=0;
-				
-				echo "<table class='table table-hover'>";
-				echo "<tr>";
-					echo "<th>".$texto['articulo']."</th>";
-					echo "<th>Descripción</th>";
-					echo "<th>".$texto['cantidad']."</th>";
-					echo "<th>".round($texto['monto'], 2)."</th>";
-					echo "<th>".round($texto['total'], 2)."</th>";
-				echo "</tr>";
-				
-				if($detalle_presupuesto)
-				{
-					foreach ($detalle_presupuesto as $row_detalle) {
-						echo "<tr>";	
-							echo "<td><a title='ver Articulo' class='btn btn-default btn-xs' href='".base_url()."index.php/articulos/articulo_abm/read/".$row_detalle->id_articulo."'>".$row_detalle->cod_proveedor."</a></td>";
-							echo "<td>".$row_detalle->descripcion."</td>";
-							echo "<td>".$row_detalle->cantidad."</td>";
-							if($row_detalle->cantidad > 0){
-								$precio = $row_detalle->precio/$row_detalle->cantidad;
-							} else {
-								$precio = 0;
-							}
-							echo "<td>$ ".round($precio, 2)."</td>";
-							$sub_total = $row_detalle->cantidad * $precio;
-							$total = $total + $sub_total;
-							echo "<td>$ ".round($sub_total,2)."</td>";
-						echo "</tr>";
-					}
+                    echo "<hr>";
+
+                    $total=0;
+
+                    echo "<table class='table table-hover'>";
+                    echo "<tr>";
+                        echo "<th>".lang('articulo')."</th>";
+                        echo "<th>".lang('descripcion')."</th>";
+                        echo "<th>".lang('cantidad')."</th>";
+                        echo "<th>".lang('monto')."</th>";
+                        echo "<th>".lang('total')."</th>";
+                    echo "</tr>";
+
+                    if($detalle_presupuesto) {
+                        foreach ($detalle_presupuesto as $row_detalle) {
+                            echo "<tr>";
+                                echo "<td><a title='ver Articulo' class='btn btn-default btn-xs' href='".base_url()."index.php/articulos/articulo_abm/read/".$row_detalle->id_articulo."'>".$row_detalle->cod_proveedor."</a></td>";
+                                echo "<td>".$row_detalle->descripcion."</td>";
+                                echo "<td>".$row_detalle->cantidad."</td>";
+                                if($row_detalle->cantidad > 0){
+                                    $precio = $row_detalle->precio/$row_detalle->cantidad;
+                                } else {
+                                    $precio = 0;
+                                }
+                                echo "<td>$ ".round($precio, 2)."</td>";
+                                $sub_total = $row_detalle->cantidad * $precio;
+                                $total = $total + $sub_total;
+                                echo "<td>$ ".round($sub_total,2)."</td>";
+                            echo "</tr>";
+                        }
+                    }
+
+                    if($interes_presupuesto) {
+                        foreach ($interes_presupuesto as $row_interes) {
+                            echo "<tr>";
+                                echo "<td>-</td>";
+                                echo "<td>".$row_interes->descripcion."</td>";
+                                echo "<td>-</td>";
+                                echo "<td>-</td>";
+                                $total = $total + $row_interes->monto;
+                                echo "<td>".$row_interes->monto."</td>";
+                            echo "</tr>";
+                        }
+                    }
+
+                    echo "<tr class='success'>";
+                        echo "<td colspan='4'>".lang('total')."</td>";
+                        echo "<th>$ ".round($total,2)."</th>";
+                    echo "</tr>";
+
+                    echo "</table>";
+
+                    echo "<hr>";
+                    echo $pie;
 				}
 				
-				if($interes_presupuesto)
-				{
-					foreach ($interes_presupuesto as $row_interes) {
-						echo "<tr>";	
-							echo "<td>-</td>";
-							echo "<td>".$row_interes->descripcion."</td>";
-							echo "<td>-</td>";
-							echo "<td>-</td>";
-							$total = $total + $row_interes->monto;
-							echo "<td>".$row_interes->monto."</td>";
-						echo "</tr>";
-					}
-				}
-				
-				echo "<tr class='success'>";	
-					echo "<td colspan='4'>".$texto['total']."</td>";
-					echo "<th>$ ".round($total,2)."</th>";
-				echo "</tr>";
-				
-				echo "</table>";
-					
-				echo "<hr>";
-				echo $pie;
-				}
-				
-				if($devoluciones)
-				{
-					$mensaje = $texto['si_devolucion']." <a class='btn btn-warning'>Ver devolución</a>";
+				if($devoluciones) {
+					$mensaje = lang('si_devolucion')." <a class='btn btn-warning'>Ver devolución</a>";
 					echo setMensaje($mensaje, 'warning');
 				}
 				
-				if($row->comentario != '')
-				{
-					if($row->com_publico == 1)
-					{
+				if($row->comentario != '') {
+					if($row->com_publico == 1) {
 						echo '<div class="well">Comentario: '.$row->comentario."</div></div>";
-					}else
-					{
+					} else {
 						echo '</div><div class="well">Comentario Privado: '.$row->comentario."</div>";
 					}					
-				}else
-				{
+				} else {
 					echo '</div>';
 				}
-			}
-			else
-			{
-				echo setMensaje($texto['no_registro'], 'success');
+			} else {
+				echo setMensaje(lang('no_registro'), 'success');
 				echo '</div>';
 			}
 			
 			
-			if(!$llamada)
-			{
+			if(!$llamada) {
 				echo "<input type='button' class='btn btn-default' value='Volver a la lista' onclick='window.history.back()'>";
 			}
 		
-  			if($row->estado != 3)
-  			{
+  			if($row->estado != 3) {
   			?>
 				<button class="btn btn-default" type="button" onclick="printDiv('printableArea')"/>
 	  				<i class="fa fa-print"></i> Imprimir
 	  			</button>
 	  			<?php 
 	  			
-	  			if(!$llamada)
-	  			{
+	  			if(!$llamada) {
 	  				// Presupuesto pendiente de pago
-		  			if($row->tipo == 2)
-		  			{
+		  			if($row->tipo == 2) {
 		  			?>
 		  			<a href="<?php echo base_url().'index.php/devoluciones/generar/'.$id_presupuesto?>" class="btn btn-default"/>
 		  				<i class="fa fa-thumbs-down"></i> Devolución
@@ -194,8 +168,7 @@
 		  			}
 	
 					// Presupuesto pagado
-		  			if($row->tipo == 1) 
-		  			{
+		  			if($row->tipo == 1) {
 		  			?>
 		  				<a href="<?php echo base_url().'index.php/presupuestos/anular/'.$id_presupuesto?>" class="btn btn-default"/>
 		  					<i class="fa fa-trash-o"></i> Anular
